c15c1b18dedecef598b946d491bf29df45530d81
DmitriyFate <ds009@DNOTEBOOK>
2021-03-19 19:54:00 +0400

    Default route

--- WebStore/Models/Employees.cs
+++ WebStore/Models/Employees.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace WebStore.Models
+﻿namespace WebStore.Models
 {
     public class Employees
     {
         public int id { get; set; }
         public string Name { get; set; }
+        public string Lastname { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
     }
 }
