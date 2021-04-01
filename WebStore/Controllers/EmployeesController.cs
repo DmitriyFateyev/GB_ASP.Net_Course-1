c4e90cbdfea49b70eb13e42a0d9a766652122f44
DmitriyFate <ds009@DNOTEBOOK>
2021-04-02 00:14:51 +0400

    CRUD Interface + Implementation

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -1,27 +1,22 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
-using WebStore.Models;
+using WebStore.Infrastructure.Interfaces;
 
 namespace WebStore.Controllers
 {
     public class EmployeesController : Controller
     {
-        private static readonly List<Employee> _Employees = new()
-        {
-            new Employee { id = 1, Name = "John", Lastname = "Wick", Patronymic = "Petrovich", Age = 53, email = "john@company.com" },
-            new Employee { id = 2, Name = "Alexander", Lastname = "Makarov", Patronymic = "Sidorovich", Age = 23, email = "alex@company.com" },
-            new Employee { id = 3, Name = "Nikolay", Lastname = "Valuev", Patronymic = "Ivanovich", Age = 32, email = "niko@company.com" }
-        };
+        private readonly IEmployeesData _employeesData;
 
+        public EmployeesController(IEmployeesData employeesData) => _employeesData = employeesData;
 
         //[Route("users")]
-        public IActionResult EmployeesList() => View(_Employees);
+        public IActionResult EmployeesList() => View(_employeesData.GetAll());
 
         //[ActionName("{id}")]                                            // not working
         public IActionResult Details(int id)
         {
-            var employee = _Employees.FirstOrDefault(e => e.id == id);
+            var employee = _employeesData.GetByID(id);
 
             if (employee == null) return NotFound();
 
---
+++ WebStore/Infrastructure/Implementations/InMemoryEmployeesData.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Implementations
+{
+    public class InMemoryEmployeesData : IEmployeesData
+    {
+        private readonly List<Employee> _EmployeesTable;
+
+        public InMemoryEmployeesData()
+        {
+            _EmployeesTable = new List<Employee>
+            {
+                new Employee { id = 1, Name = "John", Lastname = "Wick", Patronymic = "Petrovich", Age = 53, email = "john@company.com" },
+                new Employee { id = 2, Name = "Alexander", Lastname = "Makarov", Patronymic = "Sidorovich", Age = 23, email = "alex@company.com" },
+                new Employee { id = 3, Name = "Nikolay", Lastname = "Valuev", Patronymic = "Ivanovich", Age = 32, email = "niko@company.com" }
+            };
+        }
+
+        public void CreateEmployee(Employee employee)
+        {
+            employee.id = _EmployeesTable.Max(e => e.id) + 1;
+            _EmployeesTable.Add(employee);
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            var employee = GetByID(id);
+            if(employee != null)
+            {
+                _EmployeesTable.Remove(employee);
+            }
+        }
+
+        public void EditEmployee()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            return _EmployeesTable;
+        }
+
+        public Employee GetByID(int id)
+        {
+            return _EmployeesTable.FirstOrDefault(e => e.id.Equals(id));
+        }
+    }
+}
