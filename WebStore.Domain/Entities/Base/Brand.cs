1ddfd2fe9df7a55e9f51e18d16c17ad53f7f3b19
DmitriyFate <ds009@DNOTEBOOK>
2021-04-07 23:09:01 +0400

    Domain, Entites Interfaces and Base classes implementation

---
+++ WebStore.Domain/Entities/Base/Brand.cs
@@ -0,0 +1,9 @@
+﻿using WebStore.Domain.Entities.Interfaces;
+
+namespace WebStore.Domain.Entities.Base
+{
+    public class Brand : NamedEntity, IOrderedEntity
+    {
+        public int Order { get; set; }
+    }
+}
