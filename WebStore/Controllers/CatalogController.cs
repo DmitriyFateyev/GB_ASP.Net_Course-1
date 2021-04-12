b9b35f4b3a6f0a7ca3a76e350b4c162e753c8a46
DmitriyFate <ds009@DNOTEBOOK>
2021-04-13 00:44:36 +0400

    null exception

--- WebStore/Controllers/CatalogController.cs
+++ WebStore/Controllers/CatalogController.cs
@@ -12,30 +12,53 @@ namespace WebStore.Controllers
 
         public CatalogController(IProductData ProductData) => _ProductData = ProductData;
 
-        public IActionResult Index(int? BrandId, int? SectionId)
+        public IActionResult Index(int? brandId, int? sectionId)
         {
             var filter = new ProductFilter
             {
-                BrandId = BrandId,
-                SectionId = SectionId,
+                BrandId = brandId,
+                SectionId = sectionId,
             };
 
             var products = _ProductData.GetProducts(filter);
 
-            return View(new CatalogViewModel
+            var model = new CatalogViewModel()
             {
-                SectionId = SectionId,
-                BrandId = BrandId,
-                Products = products
-                   .OrderBy(p => p.Order)
-                   .Select(p => new ProductViewModel
-                   {
-                       id = p.id,
-                       Name = p.Name,
-                       Price = p.Price,
-                       ImageUrl = p.ImageUrl,
-                   })
-            });
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p => new ProductViewModel()
+                {
+                    id = p.id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+
+            return View(model);
         }
+
+        public IActionResult Shop(int? sectionId, int? brandId)
+        {
+            var products = _ProductData.GetProducts(new ProductFilter { BrandId = brandId, SectionId = sectionId });
+
+            var model = new CatalogViewModel()
+            {
+                BrandId = brandId,
+                SectionId = sectionId,
+                Products = products.Select(p => new ProductViewModel()
+                {
+                    id = p.id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price
+                }).OrderBy(p => p.Order).ToList()
+            };
+
+            return View(model);
+        }
+
     }
 }
--- WebStore/ViewModels/ProductViewModel.cs
+++ WebStore/ViewModels/ProductViewModel.cs
@@ -1,13 +1,26 @@
-﻿namespace WebStore.ViewModels
+﻿using System.Collections.Generic;
+
+namespace WebStore.ViewModels
 {
     public class ProductViewModel
     {
         public int id { get; set; }
+
+        public int Order { get; set; }
 
         public string Name { get; set; }
 
         public string ImageUrl { get; set; }
 
         public decimal Price { get; set; }
+    }
+
+    public class CatalogViewModel
+    {
+        public IEnumerable<ProductViewModel> Products { get; set; }
+
+        public int? SectionId { get; set; }
+
+        public int? BrandId { get; set; }
     }
 }
